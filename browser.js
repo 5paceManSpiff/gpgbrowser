fcf1a88314e075c00f82249dd260e70c4c8d2c3b
5paceManSpiff <alspore@gmail.com>
2015-02-21 18:16:58 -0500

    now uses gpgwrap for gpg commands

--- browser.js
+++ browser.js
@@ -1,4 +1,4 @@
-var exec = require('child_process').exec;
+var gpg = require('gpgwrap');
 var jsb = require('jsb');
 var pkg = require('./package.json');
 var recipients = [];
@@ -7,11 +7,9 @@ jsb.onReady = function(win, ipc) {
   win.loadUrl('file://' + __dirname + '/index.html');
 
   ipc.on('recipients', function(e) {
-    exec('gpg --list-public-keys | grep @ | cut -d "<" -f 2 | cut -d ">" -f 1', function(error, stdout, stderr) {
-      recipients = stdout.split('\n');
-      recipients.pop();
-      e.returnValue = recipients;
-    });
+    recipients = gpg.list();
+    console.log(recipients);
+    e.returnValue = recipients;
   });
 
   return '';
@@ -22,16 +20,7 @@ jsb.onLoad = function(win, ipc) {
 
   ipc.on('plaintext', function(e, msg, email) {
     msg = msg.replace('&&&start&&&', '');
-    exec('echo "%msg" | gpg -ea -r "%email"'.replace('%msg', msg).replace('%email', email), function(error, stdout, stderr){
-      console.log(error);
-      console.log('stdout: ' + stdout.toString());
-      console.log('stderr: ' + stderr.toString());
-      if (stderr == '') {
-        e.sender.send('encrypted', stdout, email);
-      } else {
-        e.sender.send('encrypted', 'error', email);
-      }
-    });
+    e.sender.send('encrypted', gpg.encrypt(msg, email), email);
   });
 
   return script;
